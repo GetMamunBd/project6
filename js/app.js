2cceda6d786b9e278849cf7347f91508ad514aad
GetMamunBd <53872118+GetMamunBd@users.noreply.github.com>
2021-01-27 15:52:54 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        margin: 10,
+        loop: true,
+        nav: true,
+        navText: ["<div class='nav-btn prev-slide'><i class='fas fa-chevron-left'></i></div>", "<div class='nav-btn next-slide'><i class='fas fa-chevron-right'></i></div>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            801: {
+                items: 3
+            }
+        }
+    });
+});
+
+const nav = document.querySelector('.nav-links');
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle("nav-show");
+    burger.classList.toggle("toggle");
+});
+
+
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('nav-bg');
+    } else {
+        $('nav').removeClass('nav-bg');
+    }
+})
